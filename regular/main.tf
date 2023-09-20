a1f0e3abf722ec202ac0619b6d6fbe1598055516
SharanDevOps <57943535+sharanabasavarajak@users.noreply.github.com>
2023-09-20 10:22:34 +0530

    Add files via upload

--- regular/main.tf
+++ regular/main.tf
@@ -1,1 +1,8 @@
-Test
+resource "aws_instance" "example" {
+  ami = var.ami-id
+  instance_type = var.instance-type
+  tags = {
+    "env" = "prod"
+    "Name" = "example"
+  }
+  }
---
+++ regular/terraform.tfvars
@@ -0,0 +1,2 @@
+ami-id = "ami-053b0d53c279acc90"
+instance-type = "t2.micro"
